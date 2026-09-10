2a201a94d26877c924a7c53eeb15f21e9ba4b496
Alam Wazir <bloomwaxiri@gmail.com>
2026-09-10 16:09:38 +0500

    added new endpoint

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Message(BaseModel):
 
 @app.get("/")
 def root():
-    return {"message": "API is live"}
+    return {"message": "API is live and alam is talking to you"}
 
 
 @app.get("/health")
